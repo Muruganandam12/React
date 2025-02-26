149ecdfdba2656372dc5f6682a2e804d12538ada
Muruganandam12 <murugamuruga96561@gmail.com>
2025-02-26 17:27:20 +0530

    feat:useState Hook

--- React learn/vite-project/src/App.jsx
+++ React learn/vite-project/src/App.jsx
@@ -1,15 +1,15 @@
-import User from "./User";
-
+/*import User from "./User";
+// Props
 function App(){
-  //return <h1>App</h1>
- /*react fragement 
+  return <h1>App</h1>
+ react fragement 
  return (
   <> 
   <h1>Welcome</h1>
   <h2>to my</h2>
   <h3>Website</h3>
   </>
- );*/
+ );
  const userData={
   name:"logu",
   age:22,
@@ -30,10 +30,37 @@ function App(){
   </div>
  );
 }
-/*function Hello(){
+function Hello(){
   return <h1>Hello World</h1>
 }
 
-export {Hello,App}*/
+export {Hello,App}
 export default App; 
+*/
+// Hook
 
+import React, { useState } from 'react';
+
+const App = () => {
+  const [num, setNum] = useState(1);
+  let x = 1;
+
+  const handleAdd = () => {
+    //x = x + 1; // x++; x+=1
+    //console.log("x", x);
+    //setNum(2);
+    setNum((prevNum) => {
+      return prevNum + 1;
+    });
+  };
+
+  //console.log(num);
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={handleAdd}>Add</button>
+    </div>
+  );
+};
+
+export default App;
