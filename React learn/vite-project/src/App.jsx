db1498e0fce56293db40fa4e09056a634d94df40
Muruganandam12 <murugamuruga96561@gmail.com>
2025-02-27 17:51:01 +0530

    feat:useEffect Hook

--- React learn/vite-project/src/App.jsx
+++ React learn/vite-project/src/App.jsx
@@ -37,9 +37,9 @@ function Hello(){
 export {Hello,App}
 export default App; 
 */
-// Hook
+//  useState Hook
 
-import React, { useState } from 'react';
+/*import React, { useState } from 'react';
 
 const App = () => {
   const [num, setNum] = useState(1);
@@ -59,6 +59,42 @@ const App = () => {
     <div>
       <h1>{num}</h1>
       <button onClick={handleAdd}>Add</button>
+    </div>
+  );
+};
+
+export default App;*/
+// useEffect Hook
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [num1, setNum1] = useState(100);
+  const [num2, setNum2] = useState(1000);
+
+  useEffect(() => {
+    setNum1(200);
+    console.log("from useEffect");
+
+    return () => {
+      setNum1(100);
+      console.log("memory cleaned");
+    };
+  }, []); // Empty dependency array means this effect runs only once on mount
+
+  const handleAddNum1 = () => {
+    setNum1((curr) => curr + 1);
+  };
+
+  const handleAddNum2 = () => {
+    setNum2((curr) => curr + 1);
+  };
+
+  return (
+    <div>
+      <h1>{num1}</h1>
+      <button onClick={handleAddNum1}>Add</button>
+      <h1>{num2}</h1>
+      <button onClick={handleAddNum2}>Add</button>
     </div>
   );
 };
